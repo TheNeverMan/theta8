fef673c725784898208bdb6d8a8c2c88aa1e3c62
TheNeverMan <thenevermanever@gmail.com>
2024-11-24 19:45:25 +0000

    bugfix: math mode executes not or instead of or

--- src/runtime.c
+++ src/runtime.c
@@ -372,7 +372,7 @@ static void Command_Math(struct Runtime* const Env)
     }
     case WHITE:
     {
-      Set_Var(Env,(var_1+1) % 8,~(val_2 | val_1));
+      Set_Var(Env,(var_1+1) % 8,(val_2 | val_1));
       break;
     }
     default:
